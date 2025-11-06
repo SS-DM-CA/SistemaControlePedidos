e3b128e838f075ac777154c90eff03cba99d0907
Luiz Flavio <flavioluiz219@gmail.com>
2025-11-06 20:36:50 -0300

    criação do struct Produto

---
+++ include/produto.h
@@ -0,0 +1,18 @@
+#ifndef  PRODUTO_H
+#define  PRODUTO_H
+
+typedef struct {
+    int id;
+    char descricao[100];
+    double preco;
+    int estoque;
+}Produto;
+
+
+
+
+
+
+
+
+#endif //PRODUTO_H
