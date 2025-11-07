942a4f5075ce2772dcdc116c8381994327b95d48
Luiz Flavio <flavioluiz219@gmail.com>
2025-11-06 23:00:25 -0300

    criação do struct Pedido e ItemPedido

---
+++ include/pedido.h
@@ -0,0 +1,21 @@
+#ifndef PEDIDO_H
+#define PEDIDO_H
+
+typedef struct{
+  int id;
+  int clienteid;
+  char data[11];
+  double total;
+}Pedido;
+
+typedef struct{
+  int pedidoId;
+  int peodutoId;
+  int quantidade;
+  double subtotal;
+}ItemPedido;
+
+
+
+
+#endif //PEDIDO_H
