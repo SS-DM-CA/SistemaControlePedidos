13869036b0c845f8078bc711bb48cce887caa4f0
Luiz Flavio <flavioluiz219@gmail.com>
2025-11-18 23:15:47 -0300

    excluindo o struct NomeArquivo e modificando o esqueleto da função carregarArquivo

--- include/persistencia.h
+++ include/persistencia.h
@@ -1,15 +1,9 @@
 #ifndef PERSISTENCIA_H
 #define PERSISTENCIA_H
 
-typedef struct{
 
-  char nome[100];
-  char modo;
-}NomeArquivo;
 
-NomeArquivo arqprod = {Produtos.csv}
-
-void carregarArquivo(FILE *fp,char nome[30], char modos){
+void carregarArquivo(FILE *fpro,FILE *fped, FILE *fcli,Produto *produtos, Pedido *pedidos, Cliente *clientes){
 
 }
 
