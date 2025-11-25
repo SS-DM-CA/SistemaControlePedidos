e1372a72cf37cad345d5a44c00fd845410a45e3b
Luiz Flavio <flavioluiz219@gmail.com>
2025-11-25 15:59:55 -0300

    finalizando a função consultarProduto

--- src/produto.c
+++ src/produto.c
@@ -33,5 +33,25 @@ void cadastrarProduto(int Id, Produto produto, Produto *produtos,int tamanhoProd
 
 }
 
+void consultarProduto(int Id,Produto *produtos, int tamanhoProd ) {
+    printf("Digite o id do produto:");
+    scanf("%d", &Id);
+    if (analisaProduto(Id,produtos,tamanhoProd) == 2) {
+        //produto não existe
+        //colocar mensagem de erro informando que o produto não existe
+        return EXIT_FAILURE;
+    }
+    else {
+        for (int i =0; i<tamanhoProd; i++) {
+            if (produtos[i].id == Id ) {
+                //imprimir os dados do produto
+                printf("%d,%99[^\n],%lf,%d ", produtos[i].id,produtos[i].descricao,produtos[i].preco,produtos[i].estoque);
+                break;
+            }
+        }
+    }
+
+}
+
 
 
