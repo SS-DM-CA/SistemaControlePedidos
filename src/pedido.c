e3f5afd0a5b1474684e0b29d4f1512bb4db82319
Luiz Flavio <flavioluiz219@gmail.com>
2025-11-25 17:07:35 -0300

    criação da função analisaCliente

--- src/pedido.c
+++ src/pedido.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include "../include/pedido.h"
 #include "../include/produto.h"
+#include "../include/cliente.h"
 
 int analisaPedido(int Id, Pedido *pedidos,int tamanhoPed);
+int analisaCliente(int Id, Cliente *clientes,int tamanhoCli);
 
 int main(){}
 
@@ -10,9 +12,26 @@ int analisaPedido(int Id, Pedido *pedidos,int tamanhoPed) {
     for (int i = 0; i<tamanhoPed;i++) {
         if (Id == pedidos[i].id) {
             //retorna 1 se o pedido já existir
+            //o cursor deve retornar para o campo de codigo  na tela de cadastro
             return 1;
         }
     }
     //retorna 2 se o pedido não existir
     return 2;
 }
+
+int analisaCliente(int Id, Cliente *clientes,int tamanhoCli) {
+    for (int i =0; i<tamanhoCli;i++) {
+        //no struct esta Identificador, isso pode mudar, por isso aqui irei colocar id
+        if (Id == clientes[i].id) {
+            //retorna 1 se o pedido já existir
+            //o cursor deve retornar para o campo de codigo do clinte na tela de cadastro
+            return 1;
+        }
+    }
+    //retorna 2 se o pedido não existir
+    return 2;
+}
+
+
+
