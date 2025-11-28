121b5ed7a93a384296a0a27aed308026c693b9ee
SS-DM-CA <guilhermenunes.silva15@gmail.com>
2025-11-28 13:39:55 -0300

    Correção dos structs e typedef

--- src/cliente.c
+++ src/cliente.c
@@ -1,26 +1,21 @@
-struct fisica{
+typedef struct cliente
+{
+    char identificador[5];
+    char endereco[201];
+    char telefone[15];
+    char email[101];
+}clt;
+
+typedef struct fisica{
     char nome[101];
     char celular[15];
     char cpf[12];
-};
+    clt cf;
+}fisi;
 
-struct juridica{
+typedef struct juridica{
     char razao[201];
     char contato[101];
     char cnpj[15];
-};
-
-struct cliente
-{
-    int identificador;
-    char rua[101];
-    char setor[51];
-    char cidade[51];
-    char estado[3];
-    char telefone[15];
-    char email[101];
-    union{
-        struct fisica pf;
-        struct juridica pj;
-    };
-};
+    clt cj;
+}juri;
