6377228a910e6862592136741036d9191935506b
Luiz Flavio <flavioluiz219@gmail.com>
2025-11-30 14:47:42 -0300

    corrigindo erros

--- include/produto.h
+++ include/produto.h
@@ -1,22 +1,24 @@
 #ifndef  PRODUTO_H
 #define  PRODUTO_H
 
-typedef struct {
+//#include"../include/pedido.h"
+typedef struct ItemPedido ItemPedido;
+typedef struct Produto{
     int id;
     char descricao[100];
     double preco;
     int estoque;
 }Produto;
 
-void cadastrarProduto(Produto produto,Produto *produtos,int tamanhoProd);
+void cadastrarProduto(Produto produto,Produto **produtos,int *tamanhoProd);
 
 void consultarProduto( Produto *produtos, int tamanhoProd);
 
-void removerProduto(Produto *produtos, int tamanhoProd);
+void removerProduto(Produto **produtos, int *tamanhoProd,ItemPedido *itemsPedidos, int tamanhoItemPed);
 
 void listarProdutos(Produto *produtos, int tamanhoProd);
 
-
+int analisaProduto(int Id,Produto *produtos,int tamanhoProd);
 
 
 
--- src/produto.c
+++ src/produto.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-#include "..include/produto.h"
+#include <stdlib.h>
+#include "produto.h"
+#include "produto.h"
+#include "pedido.h"
+#include "cliente.h"
 
-int analisaProduto(int Id,Produto *produtos,int tamanhoProd);
 
-int main(){}
+
+
 
 void removerProduto(Produto **produtos, int *tamanhoProd,ItemPedido *itemsPedidos, int tamanhoItemPed) {
     int Id;
@@ -11,13 +15,13 @@ void removerProduto(Produto **produtos, int *tamanhoProd,ItemPedido *itemsPedido
     scanf("%d", &Id);
     if (analisaProduto(Id,*produtos,*tamanhoProd) == 2) {
         //O produto não existe
-        return EXIT_FAILURE;
+
         printf("O produto não existe");
     }
     else {
         //verificar se tem algum pedido que contenha essa produto
         for (int i =0;i<tamanhoItemPed;i++) {
-            if (itemsPedido[i].produtoId == Id) {
+            if (itemsPedidos[i].produtoId == Id) {
                 //existe um pedido com esse produto
                 printf("Esse produto nao podera ser excluido");
                 break;
@@ -28,23 +32,23 @@ void removerProduto(Produto **produtos, int *tamanhoProd,ItemPedido *itemsPedido
                 printf("Deseja comfirmar a exclusao do produto: (s)para sim e (n)para nao");
                 scanf("%c",&confirmacao);
                 //ele quer remover o produto
-                if (confirmacao == "s") {
+                if (confirmacao == 's') {
                     //remover produto
                     for (int i=0;i<*tamanhoProd;i++) {
-                        if (*produtos[i].id==Id) {
+                        if ((*produtos)[i].id==Id) {
                             for (int j =i;j<*tamanhoProd-1;j++) {
-                                *produtos[j] = *produtos[j+1];
+                                (*produtos)[j] = (*produtos)[j+1];
                             }
                             //diminui o tamanho do vetor
                             *produtos= (Produto *) realloc(*produtos,(*tamanhoProd-1)*sizeof(Produto));
-                            (*tamanho)--;
+                            (*tamanhoProd)--;
                             break;
                         }
                     }
                 }
-                break;
+
                 //ele não quer remover  o produto
-                else if (confirmacao == "n") {
+                else if (confirmacao == 'n') {
                     printf("A remocao do produto foi cancelada");
                     break;
                 }
@@ -76,9 +80,10 @@ void cadastrarProduto (Produto produto, Produto **produtos,int *tamanhoProd) {
     int Id;
     printf("Digite o id do produto:");
     scanf("%d", &Id);
-    if (analisaProduto(Id,produtos,tamanhoProd) == 1) {
+    if (analisaProduto(Id,*produtos,*tamanhoProd) == 1) {
         //produto ja existe
-        return EXIT_FAILURE;
+
+        printf("O produto já existe ");
     }
     else {
         //Produto ainda não existe
@@ -88,7 +93,7 @@ void cadastrarProduto (Produto produto, Produto **produtos,int *tamanhoProd) {
         scanf("%99[^\n] %lf %d",produto.descricao,&produto.preco,&produto.estoque);
         //Quando ele apertar o botão Cadastrar
         //aumenta a posição do vetor
-        *produtos = (Produto *) realloc(*produtos,(*tamnhoProd+1)*sizeof(Produto));
+        *produtos = (Produto *) realloc(*produtos,(*tamanhoProd+1)*sizeof(Produto));
         //adiciona os valores no vetor
         *produtos[*tamanhoProd] = produto;
         //aumenta um no tamanho
@@ -105,7 +110,7 @@ void consultarProduto(Produto *produtos, int tamanhoProd ) {
         //produto não existe
         //colocar mensagem de erro informando que o produto não existe
         printf("Não existe esse produto");
-        return EXIT_FAILURE;
+
 
     }
     else {
@@ -113,7 +118,7 @@ void consultarProduto(Produto *produtos, int tamanhoProd ) {
         for (int i =0; i<tamanhoProd; i++) {
             if (produtos[i].id == Id ) {
                 //imprimir os dados do produto
-                printf("%d,%99[^\n],%lf,%d ", produtos[i].id,produtos[i].descricao,produtos[i].preco,produtos[i].estoque);
+                printf("%d,%s,%lf,%d ", produtos[i].id,produtos[i].descricao,produtos[i].preco,produtos[i].estoque);
                 break;
             }
         }
@@ -123,7 +128,7 @@ void consultarProduto(Produto *produtos, int tamanhoProd ) {
 
 void listarProduto (Produto *produtos,int tamanhoProd) {
     for (int i =0; i<tamanhoProd;i++) {
-        printf("%d,%99[^\n],%lf,%d ", produtos[i].id,produtos[i].descricao,produtos[i].preco,produtos[i].estoque);
+        printf("%d,%s,%lf,%d ", produtos[i].id,produtos[i].descricao,produtos[i].preco,produtos[i].estoque);
     }
 }
 
